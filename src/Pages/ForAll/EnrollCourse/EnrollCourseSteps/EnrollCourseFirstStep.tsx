ed71309896eef1ce6292612252a24a667098609e
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-03-12 05:17:30 +0600

    Applied promocode

--- src/Pages/ForAll/EnrollCourse/EnrollCourseSteps/EnrollCourseFirstStep.tsx
+++ src/Pages/ForAll/EnrollCourse/EnrollCourseSteps/EnrollCourseFirstStep.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "./EnrollCourseFirstStep.css";
+import { useState, useEffect } from "react";
 import { useGetSpecificBatchUnderCourseQuery } from "../../../../redux/api/features/Batch/batchManagementApi";
 import { useAppSelector } from "../../../../redux/hook";
 import { calculateDiscountedPrice } from "../../../../utils/Fucntion/calculateDiscount";
 import { verifyToken } from "../../../../utils/Fucntion/verifyToken";
-import { TBatch } from "../../../../utils/types/globalTypes";
+import { TBatch, TPromoCode } from "../../../../utils/types/globalTypes";
+import { useGetSpecificPromoCodeQuery } from "../../../../redux/api/features/PromoCode/promoCodeManagementApi";
 
 interface IProps {
   courseId: string;
@@ -13,7 +16,6 @@ interface IProps {
   courseStartDate: string;
   coursePrice: number;
   courseDiscount: number;
-
   activeStep: number;
   setActiveStep: React.Dispatch<React.SetStateAction<number>>;
 }
@@ -26,29 +28,62 @@ const EnrollCourseFirstStep = ({
   courseStartDate,
   coursePrice,
   courseDiscount,
-}: // activeStep,
-// setActiveStep,
-IProps) => {
-  const { data, isLoading } = useGetSpecificBatchUnderCourseQuery(courseId);
+}: IProps) => {
+  const { data: promocodeData, isLoading: PromoLoading } =
+    useGetSpecificPromoCodeQuery(import.meta.env.VITE_PROMOCODE_ID);
+  const promoData: TPromoCode = promocodeData?.data;
 
+  const { data, isLoading: batchLoading } =
+    useGetSpecificBatchUnderCourseQuery(courseId);
   const onGoingBatch: TBatch = data?.data;
-  console.log("On Going Batch: ", onGoingBatch);
 
   const { token } = useAppSelector((state) => state.auth);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   let student: any;
   if (token) {
     student = verifyToken(token);
   }
 
-  console.log("Student ===========", student);
+  const [couponCode, setCouponCode] = useState("");
+  const [appliedDiscount, setAppliedDiscount] = useState(0);
+  const [isCouponApplied, setIsCouponApplied] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [totalPrice, setTotalPrice] = useState<number>(0); // New state for total price
 
-  const appyCouponCode = () => {
-    console.log("Apply Coupon Code");
+  const initialTotalPrice = calculateDiscountedPrice(
+    coursePrice,
+    courseDiscount
+  );
+
+  // Update total price based on applied discount
+  useEffect(() => {
+    const updatedPrice =
+      initialTotalPrice - (appliedDiscount / 100) * initialTotalPrice;
+    setTotalPrice(updatedPrice);
+  }, [appliedDiscount, initialTotalPrice]);
+
+  const applyCouponCode = () => {
+    if (
+      promoData?.promoStatus === "yes" &&
+      couponCode === promoData?.promoCode
+    ) {
+      const newDiscount = promoData.promoPercent;
+      setAppliedDiscount(newDiscount);
+      setIsCouponApplied(true);
+      setErrorMessage(""); // Clear error message if promo code is valid
+      setSuccessMessage("Successfully promocode Applied");
+    } else {
+      setErrorMessage("Invalid or inactive promo code"); // Display error message if promo code is invalid
+      setSuccessMessage("");
+    }
   };
 
+  if (batchLoading || PromoLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center   bg-[#2D3035] text-white underline-offset-8 px-10">
+    <div className="flex flex-col items-center justify-center bg-[#2D3035] text-white px-10">
       <h1 className="text-3xl font-bold text-center text-white mb-6 underline">
         Enroll Course
       </h1>
@@ -56,26 +91,19 @@ IProps) => {
       <div className="flex w-full">
         <div className="w-[70%] flex flex-col gap-4">
           <h1 className="text-4xl font-bold italic">{courseTitle}</h1>
-          <p className="text-xl font-bold ">
-            Course duration: <span>{courseDuration}</span>
-          </p>
-          <p className="text-xl font-bold ">
-            Start Date: <span>{courseStartDate}</span>
-          </p>
+          <p className="text-xl font-bold">Course duration: {courseDuration}</p>
+          <p className="text-xl font-bold">Start Date: {courseStartDate}</p>
         </div>
-        <div className="w-[30%} ">
+        <div className="w-[30%]">
           <img src={courseImage} className="w-[350px] rounded-md" alt="" />
         </div>
       </div>
 
-      <div className="">
-        <h1 className="text-3xl font-bold text-center text-white my-4 underline">
+      <div className="w-full">
+        <h1 className="text-3xl font-bold text-center my-4 underline">
           Student Detail
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {/* Course Name */}
-
-          {/* Student ID */}
           <div>
             <label className="block text-sm font-medium text-gray-300 mb-1">
               Student ID
@@ -83,15 +111,10 @@ IProps) => {
             <input
               defaultValue={student?.studentId}
               type="text"
-              name="studentId"
-              className="mt-1 block w-full px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
-              placeholder="Enter student ID"
-              required
+              className="px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md"
               disabled
             />
           </div>
-
-          {/* Student Name */}
           <div>
             <label className="block text-sm font-medium text-gray-300 mb-1">
               Student Name
@@ -99,14 +122,10 @@ IProps) => {
             <input
               defaultValue={student?.name}
               type="text"
-              name="studentName"
-              className="mt-1 block w-full px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
-              placeholder="Enter student name"
+              className="px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md"
               disabled
             />
           </div>
-
-          {/* Batch ID */}
           <div>
             <label className="block text-sm font-medium text-gray-300 mb-1">
               Batch ID
@@ -114,14 +133,10 @@ IProps) => {
             <input
               defaultValue={onGoingBatch?.batchId}
               type="text"
-              name="batchId"
-              className="mt-1 block w-full px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
-              placeholder="Enter batch ID"
+              className="px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md"
               disabled
             />
           </div>
-
-          {/* Student Email */}
           <div>
             <label className="block text-sm font-medium text-gray-300 mb-1">
               Student Email
@@ -129,61 +144,62 @@ IProps) => {
             <input
               defaultValue={student?.email}
               type="email"
-              name="studentEmail"
-              className="mt-1 block w-full px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
-              placeholder="Enter student email"
-              disabled
-            />
-          </div>
-
-          {/* Student Phone */}
-          <div>
-            <label className="block text-sm font-medium text-gray-300 mb-1">
-              Student Phone
-            </label>
-            <input
-              defaultValue={student?.phone}
-              type="tel"
-              name="studentPhone"
-              className="mt-1 block w-full px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
-              placeholder="Enter student phone"
+              className="px-4 py-2 bg-gray-700 text-white border border-gray-600 rounded-md"
               disabled
             />
           </div>
         </div>
       </div>
-      <div className=" w-full h-full">
-        <h1 className="text-3xl font-bold text-center text-white my-4 underline ">
+
+      <div className="w-full h-full">
+        <h1 className="text-3xl font-bold text-center my-4 underline">
           Payment Detail
         </h1>
-        <div className=" flex items-start w-full  ">
-          <div className="w-1/2 leading-8 ">
-            <h1 className="text-[18px]">Course Price: ${coursePrice} ৳ </h1>
-            <h1 className="text-[18px]">Discount: ${courseDiscount} ৳</h1>
-            <h1 className="text-[18px]">
-              Total Price:{" "}
-              {calculateDiscountedPrice(coursePrice, courseDiscount)}৳
-            </h1>
-          </div>
-          <div className=" w-1/2 leading-8">
+        <div className=" w-full">
+          <div className="w-full leading-8">
             <div className="flex items-center gap-2">
-              <h1 className="text-[18px]">Coupon </h1>
+              <h1 className="text-[18px]">Coupon</h1>
               <input
                 type="text"
-                name="studentPhone"
-                className="   px-4  bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
+                value={couponCode}
+                onChange={(e) => setCouponCode(e.target.value)}
+                className="px-4 bg-gray-700 text-white border border-gray-600 rounded-md"
                 placeholder="Enter Coupon Code"
+                disabled={isCouponApplied}
               />
+              <button
+                className="btn btn-sm applyCoupon mt-2"
+                onClick={applyCouponCode}
+                disabled={isCouponApplied}
+              >
+                {isCouponApplied ? "Applied" : "Apply"}
+              </button>
             </div>
-            <button
-              className="btn btn-sm applyCoupon mt-2"
-              onClick={() => appyCouponCode()}
-            >
-              Apply
-            </button>
+
+            {errorMessage && (
+              <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+            )}
+            {successMessage && (
+              <p className="text-green-500 text-sm mt-2">{successMessage}</p>
+            )}
+          </div>
+
+          <div className="w-full leading-8">
+            <h1 className="text-[18px]">Course Price: {coursePrice} ৳</h1>
+            <h1 className="text-[18px]">Discount: {courseDiscount} ৳</h1>
+            <h1 className="text-[18px]">
+              Total Price: {totalPrice}৳{" "}
+              <span className="text-[12px] text-green-500">
+                (এই Ammount টাই আমাদের bKash/Nagad এ send money করুন)
+              </span>
+            </h1>
           </div>
         </div>
       </div>
+
+      <button className="btn btn-lg bg-purple-500 text-white mt-6">
+        Submit
+      </button>
     </div>
   );
 };
